4e7cbf2bf63e10a7fc6220f2d2debbd2768075b2
berkee25 <berkeozdemir25@gmail.com>
2025-11-22 13:46:54 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,52 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+public class Main {
+    public static void main(String[] args) {
+        Cat newCat = new Cat();
+        Cat newCat2 = new Cat(5, 45, "Lucy");
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+        newCat.move();
+        newCat2.move2(5);
+
+        newCat.status();
+        newCat2.status();
+
+        System.out.println("Before creating objects: " + Cylinder.getNoOfCylinder());
+        System.out.println();
+
+        Cylinder c1 = new Cylinder();
+        System.out.println("After creating first object " + c1.getNoOfCylinder());
+        System.out.println("radius: " + c1.r + " height: " + c1.h);
+        System.out.println();
+
+        Cylinder c2 = new Cylinder(10, 20);
+        System.out.println("After creating second object " + c2.getNoOfCylinder());
+        System.out.println("Base Area: " + c2.calculateBaseArea());
+        System.out.println("Surface Area: " + c2.calculateSurfaceArea());
+        System.out.println("Volume: " + c2.calculateVolume());
+        System.out.println("Mass: " + c2.calculateMass(5.12));
+
+        Person p = new Person();
+        p.setName("Selim");
+        p.setAge(16);
+        System.out.println("Person name: " + p.getName());
+        System.out.println("Person id: " + p.getId());
+        System.out.println("Person age: " + p.getAge());
+
+
+        Product p1 = new Product("Iphone 17", 23, 78000);
+        Product p2 = new Product();
+
+        System.out.println("Product1 id : " + p1.getProductId());
+        System.out.println("Product2 id : " + p2.getProductId());
+
+        System.out.println("Name: " + p1.getName());
+        System.out.println("Quantity: " + p1.getQuantity());
+        System.out.println("Price:" + p1.getPrice());
+        System.out.println("Item Value:" + p1.getItemValue());
+
+        p2.setName("Galaxy Z Fold7");
+        p2.setQuantity(20);
+        p2.setPrice(50000);
+
+        System.out.println("Second product information: " + p2.toString());
+    }
 }
