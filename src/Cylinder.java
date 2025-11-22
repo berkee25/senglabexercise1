4e28933734c95c534b254a3826d4d7900fadc852
berkee25 <berkeozdemir25@gmail.com>
2025-11-22 13:46:41 +0300

    Update Cylinder.java

--- src/Cylinder.java
+++ src/Cylinder.java
@@ -1,4 +1,35 @@
-package PACKAGE_NAME;
-
 public class Cylinder {
+    public double r;
+    public double h;
+    public static int noOfCylinder = 0;
+
+    public Cylinder() {
+        this(3, 4);
+    }
+
+    public Cylinder(double r, double h) {
+        this.r = r;
+        this.h = h;
+        noOfCylinder++;
+    }
+
+    public static int getNoOfCylinder() {
+        return noOfCylinder;
+    }
+
+    double calculateBaseArea() {
+        return r*r*Math.PI;
+    }
+
+    double calculateSurfaceArea() {
+        return 2*Math.PI*r*(r+h);
+    }
+
+    double calculateVolume() {
+        return Math.PI*r*r*h;
+    }
+
+    double calculateMass(double density) {
+        return calculateVolume()*density;
+    }
 }
