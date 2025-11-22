139adf626f9430ee5ab35a8ea91242d7e904e440
berkee25 <berkeozdemir25@gmail.com>
2025-11-22 13:46:31 +0300

    Update Cat.java

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,31 @@
-package PACKAGE_NAME;
+class Cat {
+    public int x;
+    public double direction;
+    public String name;
 
-public class Cat {
+    Cat() {
+        this(0, 180.0, "");
+    }
+
+    Cat(int x, double direction, String name) {
+        this.x = x;
+        this.direction = direction;
+        this.name = name;
+    }
+
+    void move() {
+        x += 20;
+    }
+
+    void move2(int a) {
+        direction += a;
+    }
+
+    void turn(double angle) {
+        direction += angle;
+    }
+
+    void status() {
+        System.out.println("Name: " + name + " X: " + x + " direction: " + direction);
+    }
 }
